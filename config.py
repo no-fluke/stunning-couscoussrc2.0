48f976e6ac5bc909e6b3f1fa6d410e3b2afa52e1
no-fluke <berlin14151617@gmail.com>
2026-01-03 19:03:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,12 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
 
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
+
+# Add download configuration
+DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "/app/downloads")
+MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE", 2147483648))  # 2GB
+COOLDOWN_SECONDS = int(os.environ.get("COOLDOWN_SECONDS", 30))  # 30 second cooldown
+
 # Rexbots
 # Don't Remove Credit
 # Telegram Channel @RexBots_Official
